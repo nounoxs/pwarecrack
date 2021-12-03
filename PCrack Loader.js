40b1e1d3721303501a775aea0a853a660dc9013c
nounoxs <95464847+nounoxs@users.noreply.github.com>
2021-12-03 12:29:08 +0300

    [PCrack Loader 0.1a]

---
+++ PCrack Loader.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name Krunker  PETIUKACRACKware Loader
+// @description   Krunker software [cracked by UNO XS]
+// @version       1.5.2
+// @author        UNO XS
+// @iconURL       https://cdn.discordapp.com/attachments/747504225205878814/834058167594778754/tenor.gif
+// @match         *://krunker.io/*
+// @exclude       *://krunker.io/editor*
+// @exclude       *://krunker.io/social*
+// @run-at        document-start
+// @grant         none
+// ==/UserScript==
+
+/* eslint-env es6 */
+/* eslint-disable curly, no-undef, no-loop-func, no-return-assign, no-sequences */
+
+/* Very cool loader */
+function Load() {
+  console.log("[PCrack Loader] Downloading script...")
+  var LoadP = document.createElement('script');
+  LoadP.src = 'https://rawcdn.githack.com/nounoxs/pwarecrack/d16e286993fb44b99533cf926578442ad0e673ce/pware.js';
+  document.head.append(LoadP);
+}
+Load()
